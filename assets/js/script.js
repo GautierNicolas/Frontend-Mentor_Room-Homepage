3774cb1fba9d66e7255f38dd03f20f03dbc2fd04
Nicolas Gautier <nicolas.gautier@lapiscine.pro>
2023-09-13 23:13:29 +0200

    Caroussel - ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const slides = document.querySelectorAll('.top-img')
+const textSlides = document.querySelectorAll('.slide-text')
 const rightSlide = document.getElementsByClassName('right-swipe-btn')
 const leftSlide = document.getElementsByClassName('left-swipe-btn')
-let currentSlide = 0
 
 let index = 0
 
-function getNextImage() {
+function getNext() {
     console.log(index)
     index++
     if(index >= slides.length) {
@@ -13,30 +13,35 @@ function getNextImage() {
     }
 }
 
-function getPreviousImage() {
+function getPrevious() {
     index--
     if(index <= 0) {
         index = 2
     }
-
 }
-
 
 function clickToSlide() {
     for (let slide of slides) {
         slide.classList.remove('slide-active')
     }
     slides[index].classList.add('slide-active')
-
 }
 
+function clickToSlideText(){
+    for(let text of textSlides){
+        console.log("🚀 ~ file: script.js:32 ~ clickToSlideText ~ text:", text)
+        text.classList.remove('slide-text-active')
+    }
+    textSlides[index].classList.add('slide-text-active')
+}
 
 rightSlide[0].addEventListener('click', function() {
-    getNextImage()
+    getNext()
     clickToSlide()
+    clickToSlideText()
 })
 
 leftSlide[0].addEventListener('click', function() {
-    getPreviousImage()
+    getPrevious()
     clickToSlide()
 })
